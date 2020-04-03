8eee6f014018235a27aad23b3cb35d2c8898bd5c
PAlamirpour <P.Alamirpour@gmail.com>
2020-04-03 15:36:20 +0430

    User can insert data. e.g. s_x, s_y, e_x, e_y

--- src/main.cpp
+++ src/main.cpp
@@ -56,11 +56,24 @@ int main(int argc, const char **argv)
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
 
+    float start_x = 0;
+    float start_y = 0;
+    float end_x = 0;
+    float end_y = 0;
+    std::cout << "Enter start point: " << "Start x: " << std::endl;
+    std::cin >> start_x;
+    std::cout << "Enter start point: " << "Start y: " << std::endl;
+    std::cin >> start_y;
+    std::cout << "Enter end point: " << "End x: " << std::endl;
+    std::cin >> end_x;
+    std::cout << "Enter end point: " << "End y: " << std::endl;
+    std::cin >> end_y;
+
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
